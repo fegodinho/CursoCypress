8430aa6f290be72857853156c887df9baf479e19
Felipe Godinho <fegodinho@gmail.com>
2020-10-20 14:52:20 -0300

    Aula 11 - Links

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -8,4 +8,15 @@ describe('Working with basic elements', () => {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it('Links', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
 })
