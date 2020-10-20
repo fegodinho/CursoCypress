91e012613508f2a1dbda4c8500aec6c0e0308502
Felipe Godinho <fegodinho@gmail.com>
2020-10-20 15:46:56 -0300

    Aula 15 - Checkbox

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -60,4 +60,14 @@ describe('Working with basic elements', () => {
 
         cy.get("[name='formSexo']").should('have.length', 2)
     })
+
+    it.only('Checkbox', () => {
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked')
+
+        cy.get("[name='formComidaFavorita']").click({multiple: true})
+        cy.get('#formComidaPizza').should('not.be.checked')
+        cy.get('#formComidaVegetariana').should('be.checked')
+    })
 })
