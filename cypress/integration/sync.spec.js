609d0a23e209360a7789253302c55057ede56507
Felipe Godinho <fegodinho@gmail.com>
2020-10-21 11:49:53 -0300

    Aula 20 - Buscas

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -23,8 +23,23 @@ describe('Esperas...', () => {
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').should('not.exist')
         cy.get('#novoCampo') //não encadear assertivas opostas, separe-as:  por isso o should not exist está acima
-            .should('exist')
+            .should('exist') //o cypress reexecuta todas as assertivas em caso de falha
             .type('funciona')
+    })
+
+    it.only('Uso do find...', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+        //se uma assertiva falha, o comando anterior é re-executado, porem neste caso, o find estava
+        //atrelado ao escopo do get que não foi re-executado e portanto o item da lista nao foi encontrado
+        //a solucao foi separar os comandos num novo escopo de modo que o get refaça a busca pelo elemento
+        // cy.get('#lista li')
+        //     .find('span')
+        //     .should('contain', 'Item 2')
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
     })
     
 })
