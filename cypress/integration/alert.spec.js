c981cb66777c8d41acacbc7243e7f613a2f4aec4
Felipe Godinho <fegodinho@gmail.com>
2020-10-23 11:44:13 -0300

    Aula 31 - Alert

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Working with alerts...', () => {
+    //Runs once before all tests
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    
+    //Runs befire each test
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Alert...', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+})
