e753af6c91ad092cc4d0ba385eb588c30d9ce376
Felipe Godinho <fegodinho@gmail.com>
2020-10-23 15:14:34 -0300

    Aula 36 - iFrame

---
+++ cypress/integration/iframe.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+describe('Working with iframes...', () => {
+    //Runs once before all tests
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    
+    //Runs befire each test
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Deve preencher campo de texto...', () => {
+        cy.get('#frame1').then(iframe => {
+            const body = iframe.contents().find('body')
+            cy.wrap(body).find('#tfield')
+                .type('funciona')
+                .should('have.value', 'funciona')
+        })
+    })
+
+})
