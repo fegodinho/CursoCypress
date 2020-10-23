beeffa8b4e269e93ace1134de3a868ba64c18565
Felipe Godinho <fegodinho@gmail.com>
2020-10-23 14:44:44 -0300

    Aula 35 - Desafio: Validar mensagens

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -71,4 +71,32 @@ describe('Working with alerts...', () => {
 
         cy.get('#prompt').click()
     })
+
+    it.only('Validando mensagens...', () => {
+
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio')
+            })
+        
+        cy.get('#formNome').type('Felipe')
+        cy.get('#formCadastrar').click()
+            .then(() => {
+                expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio')
+            })
+        
+        cy.get('[data-cy=dataSobrenome]').type('Godinho')
+        cy.get('#formCadastrar').click()
+            .then(() => {
+                expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio')
+            })
+        
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+
+    })
 })
