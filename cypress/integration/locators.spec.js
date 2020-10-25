812b55f5daebc3308300a931e1e8643a90c18859
Felipe Godinho <fegodinho@gmail.com>
2020-10-25 11:55:08 -0300

    Aula 40 - Ordem de busca

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe('Working with basic elements', () => {
+    //Runs once before all tests
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    
+    //Runs befire each test
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('...', () => {
+
+    })
+
+})
