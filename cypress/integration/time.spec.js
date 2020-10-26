3678775cb6236af0a8b12d912c9928a9cb352e7c
Felipe Godinho <fegodinho@gmail.com>
2020-10-26 15:48:30 -0300

    Aula 49 - Tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -21,4 +21,26 @@ describe('Working with alerts...', () => {
         cy.get('#resultado > span').should('contain', '10/04/2012')
     })
 
+    it.only('Goes to the future...', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '16037')
+        cy.get('#resultado > span').invoke('text').should('be.gt', '1603737470836')
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.lte', '0')
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').should('be.lte', '1000')
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.gte', '5000')
+
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('be.gte', '10000')
+
+    })
+
 })
