f2c616e9727f49f31e0654bdab2a9fc7874ca745
Felipe Godinho <fegodinho@gmail.com>
2020-10-26 15:18:48 -0300

    Aula 47 - Each

--- cypress/integration/dynamic.spec.js
+++ cypress/integration/dynamic.spec.js
@@ -20,6 +20,23 @@ describe('Working with alerts...', () => {
             cy.get('#formCadastrar').click()
             cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
         })
-    })    
+    })
+    
+    it.only('Deve selcionar todos usando o Each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('#formSobrenome').type('Qualquer')
+        cy.get('[name=formSexo][value=F]').click()
+
+        cy.get('[name=formComidaFavorita]').each($el => {
+            if($el.val() != 'vegetariano')
+                cy.wrap($el).click()
+        })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        //cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+    })
 
 })
