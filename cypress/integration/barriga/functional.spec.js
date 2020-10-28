5d4d8b408fa73c28f108ff5fa52b8fcee7b35731
Felipe Godinho <fegodinho@gmail.com>
2020-10-28 15:41:43 -0300

    Aula 53 - Alterar conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -18,4 +18,15 @@ describe('Should test at functional level...', () => {
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
     })
 
+    it('Should update an account...', () => {
+        cy.get('[data-test=menu-settings]').click()
+        cy.get('[href="/contas"]').click()
+        cy.xpath("//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test=nome]')
+            .clear()
+            .type('Conta alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso')
+    })
+
 })
