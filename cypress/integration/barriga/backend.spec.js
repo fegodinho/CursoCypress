2208bb9a67a55dbf09bdabcc4183202c7a070fc2
Felipe Godinho <fegodinho@gmail.com>
2020-10-29 19:34:55 -0300

    Aula 65 - getToken

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -1,46 +1,35 @@
 /// <reference types="cypress"/>
 
 describe('Should test at functional level...', () => {
+    let token
+
     //Runs once before all tests
     before(() => {
-        //cy.login('fegodinho@godinho', '123456')
-        //cy.resetApp()
+        cy.getToken('fegodinho@godinho', '123456')
+            .then(tkn => {
+                token = tkn
+            })
     })
 
     beforeEach(() => {
         //cy.get(loc.MENU.HOME).click()
     })
 
-    it('Login...', () => {
+    it('Should create an account...', () => {
         cy.request({
             method: 'POST',
-            url: 'https://barrigarest.wcaquino.me/signin',
+            headers: {Authorization: `JWT ${token}`},
+            url: 'https://barrigarest.wcaquino.me/contas',
             body: {
-                email: "fegodinho@godinho",
-                senha: "123456",
-                redirecionar: false
-            }
-        }).its('body.token').should('not.be.empty')
-        .then(token => {
-            cy.request({
-                method: 'POST',
-                headers: {Authorization: `JWT ${token}`},
-                url: 'https://barrigarest.wcaquino.me/contas',
-                body: {
                     nome: 'Conta via rest'
                 }
-            }).as('response')
-        })
+            }).as('response')       
         
         cy.get('@response').then(res => {
             expect(res.status).to.be.equal(201)
             expect(res.body).to.have.property('id')
             expect(res.body).to.have.property('nome', 'Conta via rest')
         })
-    })
-
-    it('Should create an account...', () => {
-        
     })
 
     it('Should update an account...', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -46,3 +46,18 @@ Cypress.Commands.add('resetApp', () => {
     cy.get(loc.MENU.SETTINGS).click()
     cy.get(loc.MENU.RESET).click()
 })
+
+Cypress.Commands.add('getToken', (user, password) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://barrigarest.wcaquino.me/signin',
+        body: {
+            email: user,
+            senha: password,
+            redirecionar: false
+        }
+    }).its('body.token').should('not.be.empty')
+    .then(token => {
+        return token
+    })
+})
