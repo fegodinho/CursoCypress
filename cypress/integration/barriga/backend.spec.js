78fa0b13936ff823aac8d16e2caaa8be25ea3837
Felipe Godinho <fegodinho@gmail.com>
2020-10-29 19:25:10 -0300

    Aula 64 - Inserir conta

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -21,6 +21,22 @@ describe('Should test at functional level...', () => {
                 redirecionar: false
             }
         }).its('body.token').should('not.be.empty')
+        .then(token => {
+            cy.request({
+                method: 'POST',
+                headers: {Authorization: `JWT ${token}`},
+                url: 'https://barrigarest.wcaquino.me/contas',
+                body: {
+                    nome: 'Conta via rest'
+                }
+            }).as('response')
+        })
+        
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via rest')
+        })
     })
 
     it('Should create an account...', () => {
