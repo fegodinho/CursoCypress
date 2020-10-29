ed8f0b05b0b5352193daba95fd5fdb068ce27d46
Felipe Godinho <fegodinho@gmail.com>
2020-10-28 21:17:23 -0300

    Aula 61 - Gerenciamento de massa

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -10,6 +10,10 @@ describe('Should test at functional level...', () => {
         cy.resetApp()
     })
 
+    beforeEach(() => {
+        cy.get(loc.MENU.HOME).click()
+    })
+
     it('Should create an account...', () => {
         cy.acessarMenuConta()
         cy.inserirConta('Conta de teste')
@@ -18,7 +22,7 @@ describe('Should test at functional level...', () => {
 
     it('Should update an account...', () => {
         cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.FN_XP_BTN_ATERAR('Conta de teste')).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ATERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
@@ -28,7 +32,7 @@ describe('Should test at functional level...', () => {
 
     it('Should not create and account with same name...', () => {
         cy.acessarMenuConta()
-        cy.inserirConta('Conta alterada')
+        cy.inserirConta('Conta mesmo nome')
         cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
 
@@ -37,7 +41,7 @@ describe('Should test at functional level...', () => {
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
-        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
         cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Movimentação inserida com sucesso') 
@@ -46,13 +50,23 @@ describe('Should test at functional level...', () => {
     })
 
     it('Should get balance...', () => {
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+        //cy.wait(1000)
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value','Movimentacao 1, calculo saldo')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Movimentação alterada com sucesso') 
+
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
     })
 
     it('Should remove a transaction...', () => {
         cy.get(loc.MENU.EXTRATO).click()
-        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Desc')).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('contain', 'Movimentação removida com sucesso')
     })
 
