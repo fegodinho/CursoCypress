f9cfe10d0563519413c73eafa99ef39af2a480b8
Felipe Godinho <fegodinho@gmail.com>
2020-10-30 18:30:00 -0300

    Aula 71 - Remover movimentacao

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -140,8 +140,21 @@ describe('Should test at api level...', () => {
         })
     })
 
-    it('Should remove a transaction...', () => {
-        
+    it.only('Should remove a transaction...', () => {
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: {Authorization: `JWT ${token}`},
+            qs: {
+                descricao: 'Movimentacao para exclusao'
+            }
+        }).then(res => {
+            cy.request({
+                method: 'DELETE',
+                url: `/transacoes/${res.body[0].id}`,
+                headers: {Authorization: `JWT ${token}`}
+            }).its('status').should('be.equal', 204)
+        })        
     })
 
 })
