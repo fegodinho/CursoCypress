cb6a5c4b4d9fadb1820a2971a3dd546152ec1448
Felipe Godinho <fegodinho@gmail.com>
2020-11-07 19:15:45 -0300

    Aula 84 - Validando dados

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -182,4 +182,65 @@ describe('Should test at frontend level...', () => {
         cy.get(loc.MESSAGE).should('contain', 'Movimentação removida com sucesso')
     })
 
+    it('Should validate data to create an account...', () => {    
+        cy.route({
+            method: 'POST',
+            url: '/contas',
+            response: {
+                id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1
+            },
+            onRequest: req => {
+                expect(req.request.body.nome).to.be.empty
+                expect(req.request.headers).to.have.property('Authorization')
+            }
+        }).as('saveConta')
+
+        cy.acessarMenuConta()
+
+        cy.route({
+            method: 'GET',
+            url: '/contas',
+            response: [
+                {id: 1, nome: 'Carteira', visivel: true, usuario_id: 1},
+                {id: 2, nome: 'Banco', visivel: true, usuario_id: 1},
+                {id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1}
+            ]            
+        }).as('contasSaved')
+
+        cy.inserirConta('{CONTROL}')
+        //cy.wait('@saveConta').its('request.body.nome').should('not.be.empty')
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
+    })
+
+    it.only('Should validate data to create an account using stub...', () => {  
+        const reqStub = cy.stub()  
+        cy.route({
+            method: 'POST',
+            url: '/contas',
+            response: {
+                id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1
+            },
+            onRequest: reqStub
+        }).as('saveConta')
+
+        cy.acessarMenuConta()
+
+        cy.route({
+            method: 'GET',
+            url: '/contas',
+            response: [
+                {id: 1, nome: 'Carteira', visivel: true, usuario_id: 1},
+                {id: 2, nome: 'Banco', visivel: true, usuario_id: 1},
+                {id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1}
+            ]            
+        }).as('contasSaved')
+
+        cy.inserirConta('{CONTROL}')
+        cy.wait('@saveConta').then(() => {
+            expect(reqStub.args[0][0].request.body.nome).to.be.empty
+            expect(reqStub.args[0][0].request.headers).to.have.property('Authorization')
+        })
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
+    })
+
 })
